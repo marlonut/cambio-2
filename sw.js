09f58af82ebfb2ae9614f5b5afe33caf370897fb
marlon <tafurfabrisio04@gmail.com>
2021-08-03 11:20:02 -0500

    commit de costura

--- sw.js
+++ sw.js
@@ -1,21 +1,32 @@
-var cacheName = "hello-pwa";
-var filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"];
+const STATIC__CACHE = "static-v1";
+const DYNAMIC__CACHE = "dynamic-v1";
+const IMMUTABLE__CACHE = "immutable-v1";
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-/* Serve cached content when offline */
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./js/app.js",
+  "./css/style.css",
+  "./img/avatars/hulk.jpg",
+  "./img/avatars/ironman.jpg",
+  "./img/avatars/spiderman.jpg",
+  "./img/avatars/thor.jpg",
+  "./img/avatars/wolverine.jpg",
+  "./img/favicon.ico",
+];
+const APP_SHELL_IMMUTABLE = [
+  "https://fonts.googleapis.com/css?family=Quicksand:300,400",
+  "https://fonts.googleapis.com/css?family=Lato:400,300",
+  "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+  "./css/animate.css",
+  "./js/libs/jquery.js",
+];
+self.addEventListener("install", (e) => {
+  const cacheStatic = caches.open(STATIC__CACHE).then((cacheOp) => {
+    return cacheOp.addAll(APP_SHELL);
+  });
+  const cacheImmutable = caches.open(IMMUTABLE__CACHE).then((cacheOp) => {
+    return cacheOp.addAll(APP_SHELL_IMMUTABLE);
+  });
+  e.waitUntil(Promise.all([cacheStatic, cacheImmutable]));
 });
